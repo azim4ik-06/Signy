badd0665baef0331d1a57fe047c0892ba94e003b
Azim4ik_06 <saidmurodovazim808@gmail.com>
2024-05-21 12:13:03 +0500

    исправление адаптации и дополнение сайта

--- js/navbar.js
+++ js/navbar.js
@@ -1,11 +1,19 @@
 const navbarToggle = document.querySelector('.navbar-toggler');
+const menu = document.querySelector('#navbarMenu');
+const menuSm = document.querySelector('#navbarMenuSm');
 const collapse = document.querySelector('.collapse');
+const overlay = document.querySelector('.overlay');
+const closeNavbar = document.querySelector('.btn-X');
+const closeNavbarSm = document.querySelector('.btn-X-sm');
 const dropDown = document.querySelectorAll('.dropdown-toggle');
 const dropMenu = document.querySelectorAll('.dropdown-menu');
 
 navbarToggle.addEventListener('click', navbarShow);
 function navbarShow() {
     collapse.classList.toggle('show');
+    menu.classList.toggle('d-none');
+    menuSm.classList.toggle('d-none')
+    overlay.classList.toggle('d-none');
 }
 
 let dropShow = function(manual) {
@@ -27,3 +35,19 @@ dropDown.forEach((btn, i) => {
     dropShow(i);
   });
 });
+
+closeNavbar.addEventListener('click', closeNav);
+function closeNav() {
+    collapse.classList.toggle('show');
+    menu.classList.toggle('d-none');
+    overlay.classList.toggle('d-none');
+    menuSm.classList.toggle('d-none');
+}
+
+closeNavbarSm.addEventListener('click', closeNavSm);
+function closeNavSm(){
+  collapse.classList.toggle('show');
+  menu.classList.toggle('d-none');
+  overlay.classList.toggle('d-none');
+  menuSm.classList.toggle('d-none');
+}
